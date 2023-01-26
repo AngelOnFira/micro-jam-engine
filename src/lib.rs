396ab204485a3e010cd7d6af91560854348b2b15
Joshua Barretto <joshua.s.barretto@gmail.com>
2023-01-26 21:46:36 +0000

    Improved API

--- src/lib.rs
+++ src/lib.rs
@@ -1,21 +1,47 @@
-pub trait Game {
-    type SaveState: Default;
+use std::marker::PhantomData;
+
+pub trait Game: Sized {
+    type SaveData: Default;
     
-    fn start(state: Self::SaveState) -> Self;
+    fn init() -> Self;
     
-    fn save(&mut self) -> Self::SaveState;
-    
-    fn tick(&mut self, ctx: &mut Ctx);
+    fn tick(&mut self, ctx: &mut Ctx<Self>);
 }
 
-pub struct Ctx<'tick> {
-    pub dt: f32,
+pub struct Ctx<'tick, G: Game> {
+    pub dt: f32, // Time passed since last tick, in seconds
 
     pub input: &'tick mut Input,
     pub graphics: &'tick mut Graphics,
-    pub sound: &'tick mut Sound,
+    pub audio: &'tick mut Audio,
+    pub save: &'tick mut Save<G::SaveData>,
 }
 
 pub struct Input;
+
+impl Input {
+    //pub fn key(&self, key: Key) -> KeyState { todo!() }
+    //pub fn key_presses(&self) -> impl Iterator<Item = Key>;
+    //pub fn axis(&self, axis: Axis) -> AxisState { todo!() }
+}
+
 pub struct Graphics;
-pub struct Sound;
+
+impl Graphics {
+    // TODO: Methods for drawing shapes, sprites, perhaps even triangles, as well as getting access to the framebuffer
+}
+
+pub struct Audio;
+
+impl Audio {
+    //pub fn play(&mut self, sound: Sound) { todo!() }
+}
+
+pub struct Save<S> {
+    phantom: PhantomData<S>,
+}
+
+impl<S> Save<S> {
+    pub fn read(&mut self) -> S { todo!() }
+    pub fn write(&mut self, save: S) { todo!() }
+}

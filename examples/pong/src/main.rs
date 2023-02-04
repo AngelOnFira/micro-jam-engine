af3d2de6f666614e89cfb165bb808eaf4a9fc5a8
Forest Anderson <forestkzanderson@gmail.com>
2023-02-04 02:27:20 +0000

    Rebuilding pong

--- examples/pong/src/main.rs
+++ examples/pong/src/main.rs
@@ -43,8 +43,10 @@ impl Game for Pong {
     fn tick(&mut self, dt: f32, console: &mut Console<Self>) {
         self.time += dt;
 
+        // All numbers are in pixels, based on the size of the screen
+
         // Handle all inputs
-        for input in console.input {
+        for input in &console.input {
             match input {
                 InputEvent::KeyboardInput(input) => match input.scancode {
                     17 => {
@@ -65,96 +67,38 @@ impl Game for Pong {
         // Update the ball
         self.ball_pos += self.ball_vel * dt;
 
-        // Check if the ball has hit the paddle
-        if self.ball_pos.x < -0.9 {
-            if (self.ball_pos.y - self.player.paddle_pos).abs() < 0.1 {
-                self.ball_vel.x = -self.ball_vel.x;
-            }
+        // Check if the ball has hit the left paddle
+        if self.ball_pos.x < self.player.paddle_pos - 10.0
+            && self.ball_pos.y > self.player.paddle_pos - 10.0
+            && self.ball_pos.y < self.player.paddle_pos + 10.0
+        {
+            self.ball_vel.x = -self.ball_vel.x;
         }
 
-        // Check if the ball has hit the AI's paddle
-        if self.ball_pos.x > 0.9 {
-            if (self.ball_pos.y - self.ai.paddle_pos).abs() < 0.1 {
-                self.ball_vel.x = -self.ball_vel.x;
-            }
+        // Check if the ball has hit the right paddle
+        if self.ball_pos.x > self.ai.paddle_pos + 10.0
+            && self.ball_pos.y > self.ai.paddle_pos - 10.0
+            && self.ball_pos.y < self.ai.paddle_pos + 10.0
+        {
+            self.ball_vel.x = -self.ball_vel.x;
         }
 
         // Check if the ball has hit the top or bottom of the screen
-        if self.ball_pos.y < -0.9 || self.ball_pos.y > 0.9 {
+        if self.ball_pos.y < console.graphics.size.y as f32 / -2.0
+            || self.ball_pos.y > console.graphics.size.y as f32 / 2.0
+        {
             self.ball_vel.y = -self.ball_vel.y;
         }
 
-        // Check if the ball has gone off the left side of the screen
-        if self.ball_pos.x < -1.0 {
+        // Check if the ball has hit the left or right of the screen
+        if self.ball_pos.x < console.graphics.size.x as f32 / -2.0 {
             self.ball_pos = Vec2::new(0.0, 0.0);
             self.ball_vel = Vec2::new(0.0, 0.0);
             self.score += 1;
-        }
-
-        // Check if the ball has gone off the right side of the screen
-        if self.ball_pos.x > 1.0 {
+        } else if self.ball_pos.x > console.graphics.size.x as f32 / 2.0 {
             self.ball_pos = Vec2::new(0.0, 0.0);
             self.ball_vel = Vec2::new(0.0, 0.0);
             self.score = 0;
-        }
-
-        // Draw the ball
-        console.graphics.draw_rect(
-            Rect::new(
-                self.ball_pos - Vec2::new(0.01, 0.01),
-                self.ball_pos + Vec2::new(0.01, 0.01),
-            ),
-            Color::WHITE,
-        );
-
-        // Draw the player's paddle
-        console.graphics.draw_rect(
-            Rect::new(
-                Vec2::new(-0.9, self.player.paddle_pos - 0.1),
-                Vec2::new(-0.8, self.player.paddle_pos + 0.1),
-            ),
-            Color::WHITE,
-        );
-
-        // Draw the AI's paddle
-        console.graphics.draw_rect(
-            Rect::new(
-                Vec2::new(0.8, self.ai.paddle_pos - 0.1),
-                Vec2::new(0.9, self.ai.paddle_pos + 0.1),
-            ),
-            Color::WHITE,
-        );
-
-        // Draw the score
-        console.graphics.draw_text(
-            &format!("Score: {}", self.score),
-            Vec2::new(-0.9, 0.9),
-            Color::WHITE,
-        );
-
-        // Draw the time
-        console.graphics.draw_text(
-            &format!("Time: {:.2}", self.time),
-            Vec2::new(0.7, 0.9),
-            Color::WHITE,
-        );
-
-        // Start the ball moving
-        if self.ball_vel.x == 0.0 && self.ball_vel.y == 0.0 {
-            self.ball_vel = Vec2::new(0.5, 0.5);
-        }
-
-        // Reset the game
-        if console.input.key(Key::R).is_down() {
-            self.ball_pos = Vec2::new(0.0, 0.0);
-            self.ball_vel = Vec2::new(0.0, 0.0);
-            self.score = 0;
-            self.time = 0.0;
-        }
-
-        // Quit the game
-        if console.input.key(Key::Escape).is_down() {
-            console.quit();
         }
     }
 }

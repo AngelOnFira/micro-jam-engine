e15c21dd51d1ddee7e8cac54e9046fbef2ce81b5
Matthew Castrillon-Madrigal <matthew.castrillon@accedo.tv>
2023-02-17 16:41:57 +0000

    brick broken

---
+++ examples/ai_brick_broken/src/main.rs
@@ -0,0 +1,207 @@
+use micro_jam_engine::{
+    input::InputEvent,
+    prelude::winit::event::VirtualKeyCode,
+    vek::{num_traits::clamp, *},
+    Console, Game,
+};
+
+/// This will be an implementation of pong. It will just be drawn with
+/// rectangles, and will use simple collision detection to determine if the ball
+/// has hit the paddle or the wall. It will also use a simple AI to control the
+/// paddle.
+
+const GAME_SPEED: f32 = 1.0;
+const AI_MAX_SPEED: f32 = 40.0;
+
+struct Pong {
+    /// The position of the ball
+    ball_pos: Vec2<f32>,
+    /// The velocity of the ball
+    ball_vel: Vec2<f32>,
+    /// The player's paddle
+    player: Player,
+    /// The AI's paddle
+    ai: Player,
+    /// The score of the player
+    score: u32,
+    /// The game time
+    time: f32,
+}
+
+struct Player {
+    /// The position of the paddle
+    paddle_pos: f32,
+}
+
+impl Game for Pong {
+    const TITLE: &'static str = "Pong";
+    type SaveData = ();
+
+    fn init(console: &mut Console<Self>) -> Self {
+        Self {
+            ball_pos: Vec2::new(
+                console.graphics.width() / 2.0,
+                console.graphics.height() / 2.0,
+            ),
+            ball_vel: Vec2::new(100.0, 100.0),
+            player: Player { paddle_pos: 50.0 },
+            ai: Player { paddle_pos: 25.0 },
+            score: 0,
+            time: 0.0,
+        }
+    }
+
+    fn tick(&mut self, dt: f32, console: &mut Console<Self>) {
+        self.time += dt;
+
+        let dt = dt * GAME_SPEED;
+
+        // All numbers are in pixels, based on the size of the screen
+
+        // Check if W or S is pressed
+        if console.input.key_held(VirtualKeyCode::A) || console.input.key_held(VirtualKeyCode::Left) {
+            self.player.paddle_pos -= 100.0 * dt;
+        }
+
+        if console.input.key_held(VirtualKeyCode::D) || console.input.key_held(VirtualKeyCode::Right)
+        {
+            self.player.paddle_pos += 100.0 * dt;
+        }
+
+        // Make sure the paddle doesn't go too high
+        self.player.paddle_pos = clamp(
+            self.player.paddle_pos,
+            10.0,
+            console.graphics.height() - 15.0 -10.0,
+        );
+
+        // Set up the rectangles for the ball and paddles
+        let ball_rect = Rect::new(self.ball_pos.x, self.ball_pos.y, 10.0, 10.0);
+
+        let player_paddle_rect = Rect::new(self.player.paddle_pos, console.graphics.height() - 15.0 - 10.0 , 50.0, 10.0);
+
+        let ai_paddles = vec![
+            Rect::new(
+            console.graphics.size.x as f32 - 35.0,
+            self.ai.paddle_pos,
+            50.0,
+            10.0,
+            ),
+            Rect::new(
+            console.graphics.size.x as f32 - 75.0,
+            self.ai.paddle_pos,
+            20.0,
+            10.0,
+            ),
+            Rect::new(
+            35.0,
+            self.ai.paddle_pos,
+            10.0,
+            10.0,
+            ),
+            Rect::new(
+            console.graphics.size.x as f32 - 35.0,
+            self.ai.paddle_pos,
+            20.0,
+            10.0,
+            ),
+            Rect::new(
+            50.0,
+            self.ai.paddle_pos + 80.0,
+            10.0,
+            10.0,
+            ),
+            Rect::new(
+            20.0,
+            self.ai.paddle_pos + 60.0,
+            10.0,
+            10.0,
+            ),
+
+
+        ];
+
+
+        console.graphics.clear(0x000000);
+        for (i, ai_paddle_rect) in ai_paddles.iter().enumerate() {
+
+
+        // Update the AI's paddle
+        if self.ball_pos.y > self.ai.paddle_pos + 25.0 {
+            self.ai.paddle_pos += AI_MAX_SPEED * dt;
+        } else if self.ball_pos.y < self.ai.paddle_pos + 25.0 {
+            self.ai.paddle_pos -= AI_MAX_SPEED * dt;
+        }
+        // Make sure the paddle doesn't go too high
+        self.ai.paddle_pos = clamp(
+            self.ai.paddle_pos,
+            10.0,
+            console.graphics.height() - 10.0,
+        );
+
+        // Update the ball
+        self.ball_pos += self.ball_vel * dt;
+
+        // Check if the ball has hit the left paddle and the velocity is going
+        // left
+        if ball_rect.collides_with_rect(player_paddle_rect) && self.ball_vel.x < 0.0 {
+            self.ball_vel.x = self.ball_vel.x.abs();
+            self.ball_vel.y = (self.ball_pos.y - player_paddle_rect.center().y) * 4.0;
+        }
+
+        // Check if the ball has hit the right paddle and the velocity is going
+        // right
+        if ball_rect.collides_with_rect(*ai_paddle_rect) && self.ball_vel.x > 0.0 {
+            self.ball_vel.x = -self.ball_vel.x.abs();
+            self.ball_vel.y = (self.ball_pos.y - ai_paddle_rect.center().y) * 4.0;
+        }
+
+        // Check if the ball has hit the top or bottom of the screen
+        if self.ball_pos.y < 15.0 || self.ball_pos.y > console.graphics.height() - 15.0 - 12.0 {
+            self.ball_vel.y = -self.ball_vel.y.abs()
+                * (self.ball_pos.y - (console.graphics.height() / 2.0)).signum();
+        }
+
+        // Check if the ball has hit the left or right of the screen
+        if self.ball_pos.x < 15.0 || self.ball_pos.x > console.graphics.width() - 15.0 - 12.0 {
+            self.ball_vel.x = -self.ball_vel.x.abs()
+                * (self.ball_pos.x - (console.graphics.width() / 2.0)).signum();
+        }
+
+        // Clear the screen
+
+        // Draw the arena
+        // console.graphics.draw_rect(
+        //     Rect::new(
+        //         10.0,
+        //         10.0,
+        //         console.graphics.size.x as f32 - 25.0,
+        //         console.graphics.size.y as f32 - 25.0,
+        //     ),
+        //     0x0000FF,
+        //     false,
+        // );
+
+       
+        console.graphics.draw_rect(*ai_paddle_rect, 0xFF0000, true);
+
+        }
+
+        // Draw the score
+
+        console.graphics.draw_rect(ball_rect, 0xFFFFFF, true);
+
+        // Draw the player's paddle
+        console
+            .graphics
+            .draw_rect(player_paddle_rect, 0x00FF00, true);
+
+        // Draw the AI's paddle
+
+    } // Draw the ball
+
+}
+
+fn main() {
+    Pong::run();
+}
